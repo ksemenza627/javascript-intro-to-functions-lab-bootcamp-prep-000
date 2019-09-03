6810e95cfbe6e40533f745f9773095a83293680c
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-03 01:32:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
+
+var lowercase = "hello!"
+lowercase.toLowerCase === lowercase
